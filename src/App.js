35b31fb2154f35b7b38fb5ed2ac7c5944a5dd54d
Kyle Robertson <kylecr2000@yahoo.ca>
2024-09-09 16:09:50 -0400

    Router Addition

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import Root from "./components/Root";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+
+
 import './App.css';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={ <Root/> } >
+    <Route path='/about' element={ <About/> } />
+    <Route path='/projects' element={ <Projects/> } />
+    <Route path='/projects/:title' element={ <Project/> } />
+    <Route path='/contact' element={ <Contact/> } />
+  </Route>
+));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Kyle Robertson Portfolio</h1>
-      </header>
-    </div>
+   <RouterProvider router={router} />
   );
 }
 
